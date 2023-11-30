759515099612826d0bef37a78bf9139742cbec5d
Andrew Allison <andrewallison5211@gmail.com>
2023-11-30 15:20:00 -0700

    MODAL CONTENT

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <img class="img-fluid menu-img" src="../assets/img/Menu.png" alt="We are currently updating the menu! Please check back soon!">
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+
+<script>
+export default {
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.menu-img{
+  height: 75vh;
+  widows: 40vw;
+}
+</style>
